403e480b3d410358804e1c9acd5d705dcf9cf6d8
Seth Lakowske <lakowske@gmail.com>
2016-10-20 00:45:45 -0500

    cleanup csrs

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM gliderlabs/alpine:3.4
+FROM lachlanevenson/k8s-kubectl:latest
 
 MAINTAINER Seth Lakowske <lakowske@gmail.com>
 
 ENV CERTS=/certs
 
-RUN apk-install openssl bash
+RUN apk add --update openssl bash && rm /var/cache/apk/*
 
 ADD ./openssl.cnf /
 ADD ./worker-openssl.cnf /
@@ -12,4 +12,5 @@ ADD ./k8s-keys.sh /
 
 RUN chmod 755 /k8s-keys.sh
 
+ENTRYPOINT ["/k8s-keys.sh"]
 CMD /k8s-keys.sh
--- k8s-keys.sh
+++ k8s-keys.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 print-usage() {
-    echo "Usage: $0 <api|admin|worker <worker_fqdn> <worker_ip>>"
+    echo "Usage: $0 <api|
+                     admin|
+                     worker <worker_fqdn> <worker_ip>>|
+                     all <worker_fqdn> <worker_ip>>"
     exit 1
 }
 
@@ -9,37 +12,45 @@ CMD=$1
 WORKER_FQDN=$2
 WORKER_IP=$3
 
+MASTER_HOST=$2
 
 CERTS=/certs
 APIKEYDIR=$CERTS/etc/kubernetes/ssl
 
+ADMINDIR=$CERTS/admin
+WORKERDIR=$CERTS/workers/$WORKER_FQDN
+
 worker-keys() {
     echo "worker: $WORKER_FQDN $WORKER_IP"
+    #Need to set env variable for use in worker-openssl.cnf
     export WORKER_IP=$WORKER_IP
+    CSR=$WORKERDIR/$WORKER_FQDN-worker.csr
 
-    KEYDIR=$CERTS/$WORKER_FQDN
+    mkdir -p $WORKERDIR
 
-    mkdir -p $KEYDIR
-    /usr/bin/openssl  genrsa -out $KEYDIR/$WORKER_FQDN-worker-key.pem 2048
+    /usr/bin/openssl  genrsa -out $WORKERDIR/$WORKER_FQDN-worker-key.pem 2048
 
     #requires /worker-openssl.cnf
-    /usr/bin/openssl req -new -key $KEYDIR/$WORKER_FQDN-worker-key.pem -out $KEYDIR/$WORKER_FQDN-worker.csr -subj "/CN=$WORKER_FQDN" -config /worker-openssl.cnf
+    /usr/bin/openssl req -new -key $WORKERDIR/$WORKER_FQDN-worker-key.pem -out $CSR -subj "/CN=$WORKER_FQDN" -config /worker-openssl.cnf
 
-    /usr/bin/openssl x509 -req -in $KEYDIR/$WORKER_FQDN-worker.csr -CA $APIKEYDIR/ca.pem -CAkey $CERTS/ca-key.pem -CAcreateserial -out $KEYDIR/$WORKER_FQDN-worker.pem -days 365 -extensions v3_req -extfile /worker-openssl.cnf
+    /usr/bin/openssl x509 -req -in $CSR -CA $APIKEYDIR/ca.pem -CAkey $CERTS/ca-key.pem -CAcreateserial -out $WORKERDIR/$WORKER_FQDN-worker.pem -days 365 -extensions v3_req -extfile /worker-openssl.cnf
 
+    rm $CSR
 }
 
 admin-keys() {
-    KEYDIR=$CERTS/admin
+    CSR=$ADMINDIR/admin.csr
 
-    mkdir -p $KEYDIR
-    /usr/bin/openssl  genrsa -out $KEYDIR/admin-key.pem 2048
+    mkdir -p $ADMINDIR
+    /usr/bin/openssl  genrsa -out $ADMINDIR/admin-key.pem 2048
 
-    /usr/bin/openssl req -new -key $KEYDIR/admin-key.pem -out $KEYDIR/admin.csr -subj "/CN=kube-admin"
+    /usr/bin/openssl req -new -key $ADMINDIR/admin-key.pem -out $CSR -subj "/CN=kube-admin"
 
-    /usr/bin/openssl x509 -req -in $KEYDIR/admin.csr -CA $APIKEYDIR/ca.pem -CAkey $CERTS/ca-key.pem -CAcreateserial -out $KEYDIR/admin.pem -days 365
+    /usr/bin/openssl x509 -req -in $CSR -CA $APIKEYDIR/ca.pem -CAkey $CERTS/ca-key.pem -CAcreateserial -out $ADMINDIR/admin.pem -days 365
 
+    rm $CSR
 }
+
 
 api-keys() {
 
@@ -58,6 +69,7 @@ api-keys() {
 
     /usr/bin/openssl x509 -req -in $CSR -CA $APIKEYDIR/ca.pem -CAkey $CA_KEY -CAcreateserial -out $APIKEYDIR/apiserver.pem -days 365 -extensions v3_req -extfile /openssl.cnf
 
+    rm $CSR
 }
 
 parse-cmd() {
